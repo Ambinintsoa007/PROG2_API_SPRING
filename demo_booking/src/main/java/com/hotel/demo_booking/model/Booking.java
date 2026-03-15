0f901a61727419f0e20607d8ae32565713a77ef1
Ambinintsoa007 <hei.ambinintsoa@gmail.com>
2026-03-15 13:41:30 +0300

    feat: add model

---
+++ demo_booking/src/main/java/com/hotel/demo_booking/model/Booking.java
@@ -0,0 +1,10 @@
+package com.hotel.demo_booking.model;
+
+public class Booking {
+    public String customerName;
+    public String phoneNumber;
+    public String email;
+    public int roomNumber;
+    public String roomDescription;
+    public String date;
+}
