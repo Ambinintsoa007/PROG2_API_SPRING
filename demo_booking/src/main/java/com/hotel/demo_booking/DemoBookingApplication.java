4f7e2b40d1077fa1baf49e8debb7e4644acb35c2
Ambinintsoa007 <hei.ambinintsoa@gmail.com>
2026-03-15 08:22:20 +0300

    initialize springBoot

---
+++ demo_booking/src/main/java/com/hotel/demo_booking/DemoBookingApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.demo_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoBookingApplication.class, args);
+    }
+
+}
